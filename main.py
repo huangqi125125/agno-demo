1ae94ee6d510915babf3c09372a44c28c1333391
Eddy Huang (ext.) <Eddy.Huang@partner.bmwgroup.com>
2025-08-15 16:49:03 +0800

    init

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from agno.agent import Agent
 from agno.models.azure import AzureOpenAI
+from agno.team import Team
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.tools.googlesearch import GoogleSearchTools
+from agno.tools.yfinance import YFinanceTools
 
 
 def my_agent():
     print("Hello from agno-demo!")
-    my_instructions: list[str] = [
-        "if you need to search website for details, just summarize content for answer"
-    ]
+
     while True:
         user_input = input("请输入: >> ")
         if user_input.lower() == "exit":
             break
         else:
-            agent = Agent(
+            search_agent = Agent(
                 model=AzureOpenAI(id="o3", api_version="2025-01-01-preview", azure_deployment="o3"),
                 markdown=True,
-                instructions=my_instructions,
+                instructions=[],
                 add_history_to_messages=True,
                 tools=[DuckDuckGoTools(), GoogleSearchTools()],
+                show_tool_calls=True,
             )
 
+            finance_agent = Agent(
+                model=AzureOpenAI(id="o3", api_version="2025-01-01-preview", azure_deployment="o3"),
+                markdown=True,
+                instructions=[],
+                add_history_to_messages=True,
+                tools=[YFinanceTools()],
+                show_tool_calls=True,
+            )
+
+            agent_team = Team(
+                mode="coordinate",
+                members=[search_agent],
+                model=AzureOpenAI(id="o3", api_version="2025-01-01-preview", azure_deployment="o3"),
+                success_criteria="A comprehensive financial news report with clear sections and data-driven insights.",
+                instructions=["Always include sources", "use tables to display data"],
+                show_tool_calls=True,
+                markdown=True,
+            )
             # Print the response on the terminal
-            agent.print_response(user_input, stream=True)
+            agent_team.print_response(user_input, stream=True)
 
 
 if __name__ == "__main__":
