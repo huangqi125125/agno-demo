f6029c7285a657716642f0bd1468ef52404e9987
Eddy Huang (ext.) <Eddy.Huang@partner.bmwgroup.com>
2025-08-26 17:23:14 +0800

    init

--- playground.py
+++ playground.py
@@ -1,14 +1,24 @@
 from agno.agent import Agent
 from agno.models.azure import AzureOpenAI
 from agno.playground import Playground
+from agno.storage.sqlite import SqliteStorage
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.tools.googlesearch import GoogleSearchTools
 from agno.tools.yfinance import YFinanceTools
+
+agent_storage = SqliteStorage(
+    # store sessions in the ai.sessions table
+    table_name="agent_sessions",
+    # db_file: Sqlite database file
+    db_file="tmp/data.db",
+)
 
 search_agent = Agent(
     model=AzureOpenAI(id="o3", api_version="2025-01-01-preview", azure_deployment="o3"),
     markdown=True,
     instructions=[],
+    storage=agent_storage,
+    add_datetime_to_instructions=True,
     add_history_to_messages=True,
     tools=[DuckDuckGoTools(), GoogleSearchTools()],
     show_tool_calls=True,
@@ -18,6 +28,8 @@ finance_agent = Agent(
     model=AzureOpenAI(id="o3", api_version="2025-01-01-preview", azure_deployment="o3"),
     markdown=True,
     instructions=[],
+    storage=agent_storage,
+    add_datetime_to_instructions=True,
     add_history_to_messages=True,
     tools=[YFinanceTools()],
     show_tool_calls=True,
